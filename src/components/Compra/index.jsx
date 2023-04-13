da29c66da28de8d2de795b4a78b2054f0b7ba2cb
Guilherme Henrique <gh.dourado77@gmail.com>
2023-04-13 14:33:42 -0300

    ajustando link

--- src/components/Compra/index.jsx
+++ src/components/Compra/index.jsx
@@ -3,7 +3,6 @@ import pagamento from "../../assets/payments.svg";
 export function Compra() {
   return (
     <div className="bg-black py-24 text-white flex flex-col lg:flex-row justify-center items-center gap-12 px-4">
-      
       <div className="bg-preto text-xl flex flex-col text-center items-center space-y-4 font-bold border-4 rounded border-roxo py-12 px-8 md:w-96">
         <p className="text-3xl font-black">
           E qual o <span className="text-roxo">valor?</span>{" "}
@@ -13,12 +12,17 @@ export function Compra() {
         </p>
         <p className="opacity-60 line-through">R$ 497,00</p>
         <p>Por apenas:</p>
-        <p className="text-4xl text-roxo font-black"><span className="text-2xl font-bold text-white opacity-60">2x </span>R$ 97,00</p>
-        <div className="w-full mx-auto bg-roxo rounded-md py-4 text-xl font-black shadow-xl hover:shadow-roxoclaro hover:scale-105 text-center text-white duration-300">
-          <a href="https://pay.hotmart.com/P81373984C" target="_blank">
-            Comprar agora
-          </a>
-        </div>
+        <p className="text-4xl text-roxo font-black">
+          <span className="text-2xl font-bold text-white opacity-60">2x </span>
+          R$ 97,00
+        </p>
+        <a
+          href="https://pay.hotmart.com/P81373984C"
+          target="_blank"
+          className="w-full mx-auto bg-roxo rounded-md py-4 text-xl font-black shadow-xl hover:shadow-roxoclaro hover:scale-105 text-center text-white duration-300"
+        >
+          Comprar agora
+        </a>
         <img className="w-64" src={pagamento} alt="" />
       </div>
     </div>
--- src/components/Main/ButtonCompra/index.jsx
+++ src/components/Main/ButtonCompra/index.jsx
@@ -1,9 +1,11 @@
-export function ButtonCompra({text}){
+export function ButtonCompra({ text }) {
   return (
-    <div className="md:w-1/2 mx-auto bg-roxo rounded-md py-4 text-xl font-black shadow-xl hover:shadow-roxoclaro hover:scale-105 text-center duration-300 text-white">
-      <a href="https://pay.hotmart.com/P81373984C" target="_blank">
-        {text}
-      </a>
-    </div>
+    <a
+      href="https://pay.hotmart.com/P81373984C"
+      target="_blank"
+      className=" px-8 md:px-36 mx-auto bg-roxo rounded-md py-4 text-xl font-black shadow-xl hover:shadow-roxoclaro hover:scale-105 text-center duration-300 text-white"
+    >
+      {text}
+    </a>
   );
 }
